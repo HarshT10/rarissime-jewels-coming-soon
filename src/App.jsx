3711883d3b90b12250f44e8917aeb7b13209c197
HarshT10 <harsh.thakkar1011@gmail.com>
2024-12-13 17:24:43 +0530

    fixed a minor error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-import AboutUs from "./Pages/aboutUs";
+import AboutUs from "./Pages/AboutUs";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
